461e7ee1b9f40cbf226b0e4ed2e3801fb80e40de
Manuel A <99599986+emmanuelalafaa97@users.noreply.github.com>
2025-11-21 19:48:20 +0100

    addressed unterminated string error

--- Stocks-Updated-Scripts.py
+++ Stocks-Updated-Scripts.py
@@ -208,7 +208,7 @@ def process_and_upload_data(csv_path: str):
     cleaned_automated_data['Date'] = pd.to_datetime(cleaned_automated_data['Date'], errors='coerce', format='mixed')
     cleaned_automated_data['Date'] = cleaned_automated_data['Date'].dt.strftime('%Y-%m-%d')
 
-    print("changed date type:', clean_automated_data.shape)
+    print("changed date type:", clean_automated_data.shape)
     print(clean_automated_data.info())
     # Process numeric columns: convert to numeric, handle inf/nan.
     numeric_cols = ['Close', 'High', 'Low', 'Open', 'Volume']
