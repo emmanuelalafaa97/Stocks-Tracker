b1dedc49b40ebe5aa394860704e87a206aaffcb4
Manuel A <99599986+emmanuelalafaa97@users.noreply.github.com>
2025-11-23 20:47:27 +0100

    Imported json Stocks-Updated-Scripts.py

--- Stocks-Updated-Scripts.py
+++ Stocks-Updated-Scripts.py
@@ -8,7 +8,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
-import csv
+import csv, json
 import yfinance as yf
 import os
 from pathlib import Path
