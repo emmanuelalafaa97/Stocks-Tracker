bec90ae8c3d989385674651149f25616a8c7b36a
Manuel A <99599986+emmanuelalafaa97@users.noreply.github.com>
2025-11-23 20:29:48 +0100

    Updated Stocks-Updated-Scripts.py
    Added from google_auth_oauthlib.flow import InstalledAppFlow

--- Stocks-Updated-Scripts.py
+++ Stocks-Updated-Scripts.py
@@ -4,6 +4,7 @@
 import gspread      #note gspread  and the google.oauth2.service account to get credentials
 import pandas as pd
 from google.oauth2.service_account import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
